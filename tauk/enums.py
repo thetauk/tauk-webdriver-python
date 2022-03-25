1131d4fcd44c89c67ab36fd72e90272fe8125ca2
Nathan Krishnan <nathan.a.krishnan@gmail.com>
2022-03-25 13:22:58 -0700

    minor casing change to enum values

--- tauk/enums.py
+++ tauk/enums.py
@@ -10,24 +10,24 @@ class TestStatus(Enum):
 
 @unique
 class BrowserNames(Enum):
-    CHROME = 'chrome'
-    FIREFOX = 'firefox'
-    EDGE = 'edge'
-    SAFARI = 'safari'
+    CHROME = 'Chrome'
+    FIREFOX = 'Firefox'
+    EDGE = 'Edge'
+    SAFARI = 'Safari'
 
 
 @unique
 class AutomationTypes(Enum):
-    APPIUM = 'appium'
-    SELENIUM = 'selenium'
-    ESPRESSO = 'espresso'
-    XCUI = 'xcui'
+    APPIUM = 'Appium'
+    SELENIUM = 'Selenium'
+    ESPRESSO = 'Espresso'
+    XCTEST = 'XCTest'
 
 
 @unique
 class PlatformNames(Enum):
-    IOS = 'ios'
-    ANDROID = 'android'
-    WINDOWS = 'windows'
-    LINUX = 'linux'
-    OSX = 'osx'
+    IOS = 'iOS'
+    ANDROID = 'Android'
+    WINDOWS = 'Windows'
+    LINUX = 'Linux'
+    MACOS = 'macOS'
