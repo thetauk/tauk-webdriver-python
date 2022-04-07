258aba496f13c7c9bff329abb5f55c51fc725f46
Ajith Looka <ajdovahkiin@gmail.com>
2022-04-07 01:36:49 -0700

    added test utils to support mocking API calls to Tauk

---
+++ tests/utils.py
@@ -0,0 +1,48 @@
+import inspect
+import os
+import typing
+
+import responses
+
+from tauk.exceptions import TaukException
+from tauk.tauk_webdriver import Tauk
+
+
+def mock(urls: typing.List[str], json_responses: typing.List[object], statuses: typing.List[int]):
+    responses.RequestsMock()
+    responses.start()
+    if not isinstance(urls, list) or not isinstance(json_responses, list) or not isinstance(statuses, list):
+        raise TaukException('arguments to mock method must be of list type of same size')
+
+    if not (len(urls) == len(json_responses) == len(statuses)):
+        raise TaukException('arguments to mock method must be of list type of same size')
+
+    for i, url in enumerate(urls):
+        responses.add(responses.POST, url, json=json_responses[i], status=statuses[i])
+
+    def inner_decorator(func):
+        caller_filename = None
+        test_method_name = None
+        for frame_info in inspect.stack():
+            if all(elem in frame_info.frame.f_code.co_names for elem in ['Tauk', 'observe']):
+                caller_filename = frame_info.filename.replace(os.getcwd(), '')
+                test_method_name = frame_info.frame.f_code.co_names[-1]
+                break
+
+        def inner(*args, **kwargs):
+            result = func(*args, **kwargs)
+
+            responses.stop()
+            if callable(result):
+                ctx = Tauk().get_context()
+                test_data = ctx.test_data
+                test_suite = test_data.get_test_suite(caller_filename)
+                if test_suite:
+                    test_case = test_suite.get_test_case(test_method_name)
+                    if test_case:
+                        result(caller_filename, test_method_name, ctx, test_data, test_suite, test_case)
+            return result
+
+        return inner
+
+    return inner_decorator
