0f4e44ce1897d029ad3f4c0e00ae29f26a929f2f
Ajith Looka <ajdovahkiin@gmail.com>
2022-04-14 16:17:46 -0700

    refactored mocking utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,7 @@
 import inspect
+import logging
 import os
+import re
 import typing
 
 import responses
@@ -7,23 +9,40 @@ import responses
 from tauk.exceptions import TaukException
 from tauk.tauk_webdriver import Tauk
 
+logger = logging.getLogger('tauk')
 
-def mock(urls: typing.List[str], json_responses: typing.List[object], statuses: typing.List[int], validation=None):
+
+def enable_mocking():
+    responses.RequestsMock()
+    responses.start()
+    logger.debug('Enabled mocking')
+
+
+def disable_mocking():
+    responses.stop()
+    logger.debug('Disabled mocking')
+
+
+def register_mock_urls(urls: typing.List[str], json_responses: typing.List[object], statuses: typing.List[int]):
+    for i, url in enumerate(urls):
+        logger.debug(f'Registering mock url: {url}')
+        responses.add(responses.POST, url, json=json_responses[i], status=statuses[i])
+
+
+def mock(urls: [], json_responses: typing.List[object], statuses: typing.List[int], validation=None):
     if not isinstance(urls, list) or not isinstance(json_responses, list) or not isinstance(statuses, list):
         raise TaukException('arguments to mock method must be of list type of same size')
 
     if not (len(urls) == len(json_responses) == len(statuses)):
         raise TaukException('arguments to mock method must be of list type of same size')
 
-    responses.RequestsMock()
-    responses.start()
+    if not Tauk.is_initialized():
+        enable_mocking()
+        register_mock_urls(urls, json_responses, statuses)
 
-    for i, url in enumerate(urls):
-        responses.add(responses.POST, url, json=json_responses[i], status=statuses[i])
-
-    api_token = os.getenv('TAUK_API_TOKEN', 'api-token')
-    project_id = os.getenv('TAUK_PROJECT_ID', 'project-id')
-    Tauk(api_token=api_token, project_id=project_id)
+        api_token = os.getenv('TAUK_API_TOKEN', 'api-token')
+        project_id = os.getenv('TAUK_PROJECT_ID', 'project-id')
+        Tauk(api_token=api_token, project_id=project_id)
 
     def inner_decorator(func):
         caller_filename = None
@@ -58,7 +77,8 @@ def mock(urls: typing.List[str], json_responses: typing.List[object], statuses: 
                         if test_case:
                             validation(caller_filename, test_method_name, ctx, test_data, test_suite, test_case)
             finally:
-                responses.stop()
+                # disable_mocking()
+                pass
 
         return inner
 
@@ -67,14 +87,10 @@ def mock(urls: typing.List[str], json_responses: typing.List[object], statuses: 
 
 def mock_success(expected_run_id='6d917db6-cf5d-4f30-8303-6eefc35e7558', validation=None):
     return mock(
-        urls=[
-            f'https://www.tauk.com/api/v1/execution/project-id/initialize',
-            f'https://www.tauk.com/api/v1/execution/project-id/{expected_run_id}/report/upload'
-        ],
-        json_responses=[
-            {'run_id': expected_run_id, 'message': 'success'},
-            {'message': 'success'}
-        ],
+        urls=[re.compile(r'https://www.tauk.com/api/v1/execution/.+/initialize'),
+              re.compile(r'https://www.tauk.com/api/v1/execution/.+/.+/report/upload')],
+        json_responses=[{'run_id': expected_run_id, 'message': 'success'},
+                        {'message': 'success'}],
         statuses=[200, 200],
         validation=validation
     )
