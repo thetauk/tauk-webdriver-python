adb1c59b9d31cc67a02c86485e36c21fbd432e2f
Ajith Looka <ajdovahkiin@gmail.com>
2022-04-25 14:05:59 -0700

    handle exeception and improve logging

--- tauk/listeners/unittest_multiprocess_listener.py
+++ tauk/listeners/unittest_multiprocess_listener.py
@@ -9,7 +9,8 @@ logger = logging.getLogger('tauk')
 class TaukMultiprocessListener(TaukListener):
 
     def __init__(self, stream, descriptions, verbosity) -> None:
+        logger.debug('Initializing Tauk Multiprocess listener')
         self.multiprocess_run = True
         self.cleanup_exec_context_on_exit = False
-        Tauk(multi_process_run=True, cleanup_exec_context=self.cleanup_exec_context_on_exit)
+        Tauk(multi_process_run=self.multiprocess_run, cleanup_exec_context=self.cleanup_exec_context_on_exit)
         super().__init__(stream, descriptions, verbosity)
