e7ad4829ae93b13ea8b5993662d590f4d1c82858
Ajith Looka <ajdovahkiin@gmail.com>
2022-05-24 12:45:14 -0700

    create an instance of companion and launch it in context if a valid config exists

--- tauk/context/context.py
+++ tauk/context/context.py
@@ -5,6 +5,8 @@ import uuid
 import jsonpickle
 
 from tauk.api import TaukApi
+from tauk.companion.companion import TaukCompanion
+from tauk.companion.config import CompanionConfig
 from tauk.context.test_data import TestData
 from tauk.exceptions import TaukException
 from filelock import FileLock
@@ -14,11 +16,18 @@ logger = logging.getLogger('tauk')
 
 class TaukContext:
 
-    def __init__(self, api_token, project_id, multi_process_run=False):
+    def __init__(self, api_token, project_id, multi_process_run=False, companion_config: CompanionConfig = None):
         self.test_data: TestData = TestData()
         self.exec_dir = self._get_exec_dir(multi_process_run)
-        self.exec_file = os.path.join(self.exec_dir, 'exec.run')
+        if not os.path.exists(self.exec_dir):
+            os.makedirs(self.exec_dir)
+        self._exec_file = os.path.join(self.exec_dir, 'exec.run')
         self.api = TaukApi(api_token, project_id, multi_process_run)
+
+        # Initialize Tauk Companion
+        self.companion: TaukCompanion = None
+        if companion_config:
+            self.launch_companion(companion_config)
 
         if multi_process_run:
             self._setup_execution_file()
@@ -61,16 +70,17 @@ class TaukContext:
             return
 
         logger.debug(f'Deleting execution files in {self.exec_dir}')
-        lock_file = f'{self.exec_file}.lock'
-        if os.path.exists(self.exec_file):
-            os.remove(self.exec_file)
+        lock_file = f'{self._exec_file}.lock'
+        if os.path.exists(self._exec_file):
+            os.remove(self._exec_file)
         if os.path.exists(lock_file):
             os.remove(lock_file)
-        os.rmdir(self.exec_dir)
+        # os.rmdir(self.exec_dir)
+        # TODO: Find a safe way to delete the directory
 
     def _setup_execution_file(self):
         def set_exec_file(r_id, a_token, p_id):
-            with open(self.exec_file, "w") as f:
+            with open(self._exec_file, "w") as f:
                 logger.debug(f'Updating execution file with {r_id}')
                 f.write(f'{r_id},{a_token},{p_id}')
                 self.run_id = r_id
@@ -82,13 +92,13 @@ class TaukContext:
             except ValueError:
                 return False
 
-        if not os.path.exists(self.exec_dir):
-            os.makedirs(self.exec_dir)
+        # if not os.path.exists(self.exec_dir):
+        #     os.makedirs(self.exec_dir)
 
-        with FileLock(f'{self.exec_file}.lock', timeout=30):
-            logger.debug(f'Execution locked for {self.exec_file}')
-            if os.path.exists(self.exec_file):
-                with open(self.exec_file, 'r') as file:
+        with FileLock(f'{self._exec_file}.lock', timeout=30):
+            logger.debug(f'Execution locked for {self._exec_file}')
+            if os.path.exists(self._exec_file):
+                with open(self._exec_file, 'r') as file:
                     run_id, api_token, project_id = file.read().strip().split(',')
                     self.api.set_token(api_token, project_id)
                     if is_valid_uuid(run_id):
@@ -101,7 +111,7 @@ class TaukContext:
                         logger.warning(f'Invalid runID [{run_id}]')
 
             set_exec_file(self._init_run(), self.api.get_api_token(), self.api.get_project_id())
-            logger.debug(f'Execution unlocked for {self.exec_file}')
+            logger.debug(f'Execution unlocked for {self._exec_file}')
 
     def get_json_test_data(self, test_suite_filename, test_method_name):
         suite = self.test_data.get_test_suite(test_suite_filename)
@@ -122,3 +132,10 @@ class TaukContext:
         }
 
         return jsonpickle.encode(json_data, unpicklable=False)
+
+    def launch_companion(self, companion_config: CompanionConfig):
+        self.companion = TaukCompanion(self.api.get_api_token(), self.exec_dir, companion_config)
+        try:
+            self.companion.launch()
+        except Exception as ex:
+            logger.error('Failed to launch tauk companion', exc_info=ex)
