001e8f487f4671fe0491b310fb15acd26089d052
Ajith Looka <ajdovahkiin@gmail.com>
2022-05-25 14:26:50 -0700

    added invalid type exception

--- tauk/exceptions.py
+++ tauk/exceptions.py
@@ -6,9 +6,13 @@ logger = logging.getLogger('tauk')
 class TaukException(Exception):
     def __init__(self, msg='') -> None:
         pass
-        # logger.error(msg)
 
 
-class TaukTestMethodNotFound(TaukException):
+class TaukTestMethodNotFoundException(TaukException):
+    def __init__(self, msg='') -> None:
+        super().__init__(msg)
+
+
+class TaukInvalidTypeException(TaukException):
     def __init__(self, msg='') -> None:
         super().__init__(msg)
