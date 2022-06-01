19759802367894389f995323808feaf0fefcbfec
Ajith Looka <ajdovahkiin@gmail.com>
2022-06-01 14:16:22 -0700

    moved properties to instance variables

--- tauk/context/test_error.py
+++ tauk/context/test_error.py
@@ -1,9 +1,11 @@
 class TestError:
-    error_type: str
-    error_msg: str
-    line_number: int
-    invoked_func: str
-    code_executed: str
+
+    def __init__(self) -> None:
+        self.error_type: str = ''
+        self.error_msg: str = ''
+        self.line_number: int = 0
+        self.invoked_func: str = ''
+        self.code_executed: str = ''
 
     def __getstate__(self):
         return self.__dict__
